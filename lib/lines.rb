1909d6ba1a227dd04389e279e58ceaebd72f709c
Joshua Atteberry & Marc Lee <student@epicodus.com>
2015-01-21 11:12:22 -0800

    Add find method to search for line by id number

--- lib/lines.rb
+++ lib/lines.rb
@@ -1,8 +1,8 @@
 class Lines
-  attr_reader(:name, :id)
+  attr_reader(:line_name, :id)
 
   define_method(:initialize) do |attributes|
-    @name = attributes.fetch(:name)
+    @line_name = attributes.fetch(:line_name)
     @id = attributes.fetch(:id)
   end
 
@@ -10,10 +10,31 @@ class Lines
     returned_lines = DB.exec("SELECT * FROM lines;")
     lines = []
     returned_lines.each() do |line|
-      name = line.fetch("name")
+      line_name = line.fetch("line_name")
       id = line.fetch("id").to_i()
-      lines.push(Lines.new({:name => name, :id => id}))
+      lines.push(Lines.new({:line_name => line_name, :id => id}))
     end
     lines
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO lines (line_name) VALUES ('#{@line_name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+
+  define_method(:==) do |another_list|
+    self.line_name().==(another_list.line_name()).&(self.id().==(another_list.id()))
+  end
+
+  define_singleton_method(:find) do |id|
+    found_line = nil
+    Lines.all().each() do |line|
+      if line.id().==(id)
+        found_line = line
+      end
+    end
+    found_line
+  end
+
 end
--- spec/lines_spec.rb
+++ spec/lines_spec.rb
@@ -5,8 +5,35 @@ require('lines')
 
 describe(Lines) do
   describe(".all") do
-    it("tells you its name") do
+    it("tells you its line_name") do
       expect(Lines.all()).to(eq([]))
     end
   end
+
+  describe("#save") do
+    it("lets you save lines to the database") do
+      line = Lines.new({:line_name => "Red line", :id => nil})
+      line.save()
+      expect(Lines.all()).to(eq([line]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same line if it has the same name") do
+      line1 = Lines.new({:line_name => "Blue line", :id => nil})
+      line2 = Lines.new({:line_name => "Blue line", :id => nil})
+      expect(line1).to(eq(line2))
+    end
+  end
+
+    describe(".find") do
+    it("returns a line by its ID number") do
+      test_line = Lines.new({:line_name => "Epicodus stuff", :id => nil})
+      test_line.save()
+      test_line_2 = Lines.new({:line_name => "Home stuff", :id => nil})
+      test_line_2.save()
+      expect(Lines.find(test_line_2.id())).to(eq(test_line_2))
+    end
+  end
+
 end
